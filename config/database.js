8401457382ce389743da1b50cc1ece3f808e6cd5
Ashutosh1104 <asksingh11@gmail.com>
2020-08-23 21:47:11 +0530

    small

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-module.exports = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+module.exports = new Sequelize( process.env.DATABASE_URL, {
+    host: localhost,
     dialect: 'postgres',
 
     pool: {
