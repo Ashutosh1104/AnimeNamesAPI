d6f87c12657306f032c92fee9e8210189292c594
Ashutosh1104 <asksingh11@gmail.com>
2020-09-06 18:15:28 +0530

    get allowed in cors

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
-const express= require('express')
+const express = require('express')
 const app = express();
-var {runDBupdate} = require('./updateDb');
+var { runDBupdate } = require('./updateDb');
 app.use(express.urlencoded({ extended: false }));
 
-const PORT = process.env.PORT || 8000 ;
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+const PORT = process.env.PORT || 8000;
 
 runDBupdate();
-setInterval( runDBupdate , 48*60*60*1000) // running update every 24 hrs
+setInterval(runDBupdate, 48 * 60 * 60 * 1000) // running update every 24 hrs
 
-app.get("/" , function(req,res){
-    const result = { 
-        Message : "Please read instructions on Github" ,
-        Github : 'https://github.com/Ashutosh1104/AnimeNamesAPI/blob/master/README.md'
+app.get("/", function (req, res) {
+    const result = {
+        Message: "Please read instructions on Github",
+        Github: 'https://github.com/Ashutosh1104/AnimeNamesAPI/blob/master/README.md'
     }
     res.send(result);
 })
 
 app.use('/anime', require('./routes/anime'));
 
-app.listen( PORT , function(){
+app.listen(PORT, function () {
     console.log('server started on 8000');
 })
 
